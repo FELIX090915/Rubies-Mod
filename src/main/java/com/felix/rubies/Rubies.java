e13de2a52dd33f7df32cdb5595005ecb25e74715
Alejandro <felix090915@gmail.com>
2021-12-03 17:26:16 -0600

    Initial commit

---
+++ src/main/java/com/felix/rubies/Rubies.java
@@ -0,0 +1,11 @@
+package com.felix.rubies;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Rubies implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
